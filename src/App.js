837121bd4a379cce3ac42cf3c31fac52b2aaefa4
Fooyer <xneerd157@gmail.com>
2023-02-23 16:44:33 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 
+// Frameworks imports
+
+import { Row, Col } from 'react-bootstrap';
+
 // Components Imports
 
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 
+// App code
+
 function App() {
   return (
     <div className="App">
 
-      <Header/>
+        <Header />
 
       <div className="Conteudo">
 
