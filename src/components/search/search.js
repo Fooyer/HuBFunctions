091dbc938fefa3ca145153fbfabc405c5d215953
Fooyer <xneerd157@gmail.com>
2023-02-23 21:17:52 -0300

    supabase

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,6 +2,10 @@
 
 import "./search.css"
 
+// Importar Conexão com Banco
+
+import { createClient } from '@supabase/supabase-js'
+
 // Import Frameworks
 
 import { Col } from 'react-bootstrap';
@@ -13,6 +17,17 @@ import imgSearch from "../../images/lupa.svg"
 // Component Funciton
 
 function SearchBar(){
+
+    const supabaseUrl = "https://bfgjcqecgspzgfsfaawj.supabase.co"
+    const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmZ2pjcWVjZ3Nwemdmc2ZhYXdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcxODM3ODUsImV4cCI6MTk5Mjc1OTc4NX0.xsBsrZfNTc5huqPX2bBIGYgSfurupRzdSeW-H_OnuRQ"
+    const supabase = createClient(supabaseUrl, supabaseKey)
+
+    async function searchLanguages(){
+
+        let { data: languages, error } = await supabase.from('planguages').select('planguage')
+
+            console.log(languages)
+        }
 
     return (
 
@@ -39,7 +54,7 @@ function SearchBar(){
 
                 <Col xs="1" md="1" lg="1" className="Search-image">
 
-                    <img src={imgSearch} id="img-search" alt="function search" />
+                    <img src={imgSearch} id="img-search" alt="function search" onClick={searchLanguages}/>
                     
                 </Col>
 
