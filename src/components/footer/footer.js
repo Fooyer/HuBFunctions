ec2f86dbd523a0fee67f9a50fa0503591e2c80d8
Freddy Baier <xneerd157@gmail.com>
2023-02-24 10:43:53 -0300

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,12 @@ function Footer(){
 
     return (
         <footer className="Main-Footer">
-       
+            <h3>Made By</h3>
+            <p>
+                <a href="mailTo:freddybaier7@gmail.com">freddybaier7@gmail.com</a><br/>
+                <a href="mailTo:arthurfary22@gmail.com">arthurfary22@gmail.com</a>
+            </p>
+
 		</footer>
     )
 
