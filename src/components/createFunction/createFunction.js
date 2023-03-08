9a73e53a477b06ec257e7c188dcb5ca15558f1ea
Freddy Baier <xneerd157@gmail.com>
2023-03-08 09:24:30 -0300

    language change

--- src/components/createFunction/createFunction.js
+++ src/components/createFunction/createFunction.js
@@ -6,8 +6,10 @@ import "./createFunction.css"
 
 import { Col, Row } from "react-bootstrap";
 import AceEditor from "react-ace";
+import { useState } from "react";
 
 import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-tomorrow_night_eighties";
 import "ace-builds/src-noconflict/ext-language_tools";
 
@@ -15,8 +17,20 @@ import "ace-builds/src-noconflict/ext-language_tools";
 
 function CreateFunction(){
 
+    const [mainLanguage, setmainLanguage] = useState('javascript');
+
     function onChange(newValue) {
         console.log(newValue);
+    }
+
+    function changeLanguage(){
+
+        let linguagem = document.getElementById('selectionLanguage').value
+
+        console.log(linguagem)
+
+        setmainLanguage(linguagem)
+
     }
 
     // HTML Code
@@ -33,12 +47,13 @@ function CreateFunction(){
                         <label for="title">Título:</label>
                         <input name="tituloNewFunction" required />
 
-                        <select required>
-                            <option></option>
+                        <select onChange={changeLanguage} id="selectionLanguage" required>
+                            <option value="javascript">javascript</option>
+                            <option value="python">python</option>
                         </select>
                     </div>
 
-                        <AceEditor enableLiveAutocompletion={true} enableSnippets={true} mode="javascript" theme="tomorrow_night_eighties" onChange={onChange} editorProps={{ $blockScrolling: true }} fontSize={15} width={800} height="auto" className="textEditorFunction" />
+                        <AceEditor enableLiveAutocompletion={false} enableSnippets={false} mode={mainLanguage} theme="tomorrow_night_eighties" onChange={onChange} editorProps={{ $blockScrolling: true }} fontSize={15} width={800} height="auto" className="textEditorFunction" />
                         
                         <textarea />
 
