d6dcec274cbb1fe699d6efa72d8bc40213c6605e
Freddy Baier <xneerd157@gmail.com>
2024-03-04 08:29:11 -0300

    header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -4,13 +4,15 @@ import styles from './header.module.css';
 import Cookies from 'js-cookie';
 
 import Link from 'next/link';
+import { useState } from 'react';
 
 import Image from 'next/image';
 import imgLua from '../../../../public/lua.svg';
+import imgSol from '../../../../public/sol.svg';
 
 function Header(){
 
-    let tema = Cookies.get('tema')
+    const [tema, setTema] = useState(Cookies.get("tema"))
 
     async function alterarTema(){
         let temas = Cookies.get('tema')
@@ -19,17 +21,19 @@ function Header(){
             Cookies.set("tema",'dark', {expires: 365})
             document.getElementById('root').classList.add('dark')
             document.getElementById('root').classList.remove('light')
+            setTema("dark")
 
         } else {
             Cookies.set("tema",'light', {expires: 365})
             document.getElementById('root').classList.add('light')
             document.getElementById('root').classList.remove('dark')
+            setTema("light")
         }
     }
 
     return (
         <header className={styles.header}>
-            <nav>
+            <nav className={styles.nav}>
                 <Link href="/">
                     Home
                 </Link>
@@ -40,9 +44,10 @@ function Header(){
                     Contact
                 </Link>
             </nav>
-            <button onClick={alterarTema} className={tema === 'light' ? styles.lightBotao : styles.darkBotao}>
-                {tema === "light" && <Image src={imgLua} width={80} />}
-            </button>
+            <div onClick={alterarTema} className={tema === 'light' ? styles.lightBotao : styles.darkBotao}>
+                {tema == "light" && <Image src={imgLua} width={40} />}
+                {tema == "dark" && <Image src={imgSol} width={40} />}
+            </div>
         </header>
     )
 };
