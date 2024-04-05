28aedaad57d1b4cfa16e9f974c9a109bd18bc0a8
Gabriel Ribeiro Santos <gabriel.santos@bplus.com.br.local>
2024-04-05 11:10:46 -0300

    Router find language

---
+++ src/app/api/languages/find-language/route.jsx
@@ -0,0 +1,32 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+let db = null;
+
+export async function GET(request) {
+    const body = request[Object.getOwnPropertySymbols(request)[1]];
+    const id_language = body.url.searchParams.get("id_language");
+
+    if (!db) {
+        db = await open({
+            filename: "./collection.db",
+            driver: sqlite3.Database,
+        });
+    }
+
+    const sql = `SELECT name FROM Languages WHERE id = ${id_language};`;
+
+    const language = await db.all(sql);
+
+    if (language[0] == null) {
+        return new Response(JSON.stringify("Data not found"), {
+            headers: { "Content-Type": "application/json" },
+            status: 404,
+        });
+    }
+    
+    return new Response(JSON.stringify(language[0]), {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+    });
+}
